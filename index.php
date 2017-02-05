b7b6a4ac5cd49768cf5f58fbb0ad9c3531291456
Nathaniel Law <nathangl@sbcglobal.net>
2017-02-04 23:53:24 -0500

    css test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<html>
+<!DOCTYPE html>
 <head>
-<title>PHP Test</title>
+	<title>PHP Test</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 <?php echo '<p>Hello World</p>'; ?>
