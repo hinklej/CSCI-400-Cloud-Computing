c0d3f480c1121fa66f2c1ae3e61080c867b1e85a
Nathaniel Law <nathangl@sbcglobal.net>
2017-02-05 18:44:39 -0500

    table changes

--- table.php
+++ table.php
@@ -5,31 +5,30 @@
 </head>
 <body>
 
-<?php
-$size = 10;
-if( isset($_POST['info']))
-   $size = $_POST['info']
-?>
-
 <form method="POST">
       Enter Information:<input type="string" name="info">
       <input type="submit">
 </form>
 
- <?php
+<?php
+
+$size = 10;
+
+if( isset($_POST['info'])) {
+   $size = $_POST['info'];
+}
+
 echo 'table border = "1">',"\n";
-for($y = 0; $y = $size; $y++)
-{
- echo "<tr>\n";
-    for($x = 0; $x < $size; $x++)
-{
-		     echo "	<td>";
-		     echo $x * $y;
-		     echo "</td>\n";
+for($y = 0; $y < $size; $y++) {
+ 	echo "<tr>\n";
+	for($x = 0; $x < $size; $x++) {
+		echo "	<td>";
+		echo $x * $y;
+		echo "</td>\n";
+	}
+	echo "</tr\n";
 }
-		     echo "</tr\n";
-}
-		     echo "</table\n";
+echo "</table\n";
 
 ?>
 </body>
