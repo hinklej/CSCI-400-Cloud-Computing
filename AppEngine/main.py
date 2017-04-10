cf767045165d1af370b5a2294885b09e80d8de64
Jake Hinkle <jakehinkle13@yahoo.com>
2017-04-10 00:04:28 +0000

    main.py v2

--- AppEngine/main.py
+++ AppEngine/main.py
@@ -9,33 +9,37 @@ app.config['DEBUG'] = True
 def isprime(n):
     '''check if integer n is a prime'''
     # make sure n is a positive integer
-    n = abs(int(n))
-    
+    if n = abs(int(n))
+
     # 0 and 1 are not primes
     if n < 2:
-        return False
+            temp = False
+        return temp
     
         # 2 is the only even prime number
-    if n == 2: 
-        return True    
+    if n == 2:
+            temp = True
+        return temp   
     
         # all other even numbers are not primes
-    if not n & 1: 
-        return False
+    if not n & 1:
+            temp = False
+        return temp
     
         # range starts with 3 and only needs to go up the squareroot of n
     # for all odd numbers
     for x in range(3, int(n**0.5)+1, 2):
         if n % x == 0:
-            return False
-    return True
-    numbers = n
+            temp = False
+            return temp
+            temp = True
+    return temp
+    temp = numbers
     memcache.add('numbers', '')
-    number = 2
     if request.method == 'GET' and number_numbers in request.args:
            number = request.args['number_numbers']
-              numbers = memcache.get('numbers').split('/')
-                 if number in numbers:
+              number = memcache.get('numbers').split('/')
+                 if n in numbers
                         memcache.incr(number)
                            else
                                   memcache.set('numbers', memcache.get('names') + '/' + number)
@@ -45,6 +49,7 @@ def isprime(n):
                                    for n in numbers:
                                           primes.append( (n, memcache.get(n) ) )
                                           return render_template('primes.html', primes = primes, number = number)
+                                          
                                           
                                           
                                           
