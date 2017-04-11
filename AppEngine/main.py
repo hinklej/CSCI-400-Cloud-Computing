cec5075477d3010bcc2f347f96ce26b525a29979
Nathaniel Law <nathangl@sbcglobal.net>
2017-04-10 23:12:37 -0400

    Main.py small fixes

--- AppEngine/main.py
+++ AppEngine/main.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 primeNums = []
+isPrime = False
 
 @app.route('/')
 def isprime():
 
     user = users.get_current_user()
+    global isPrime
+    number = 199
     
     if user:
         logouturl = users.create_logout_url('/')
@@ -23,41 +26,48 @@ def isprime():
     
     # 0 and 1 are not primes
         if n < 2:
-            #return False
-            pass
+            isPrime = False
+            #return temp
+            #pass
     
         # 2 is the only even prime number
         if n == 2: 
-            #return True
-            pass   
+            isPrime = True
+            #return temp
+            #pass   
     
         # all other even numbers are not primes
         if not n & 1: 
-            #return False
-            pass
+            isPrime = False
+            #return temp
+            #pass
         
         # range starts with 3 and only needs to go up the squareroot of n
     # for all odd numbers
         for x in range(3, int(n**0.5)+1, 2):
             if n % x == 0:
+                isPrime = False
                 #return False
-                pass
-        #return True
-        numbers = n
-        memcache.add('numbers', '')
-        number = 2
-        if request.method == 'GET' and 'number_numbers' in request.args:
-               number = request.args['number_numbers']
-               number = memcache.get('numbers').split('/')
-               if number in numbers:
+                #pass
+        #temp = True
+        #return temp
+        if (isPrime == True):
+        	numbers = n
+        	memcache.add('numbers', '')
+        	number = 2
+        	if request.method == 'GET' and 'number_numbers' in request.args:
+            	    number = request.args['number_numbers']
+              	    numbers = memcache.get('numbers')
+            	   #number = memcache.get('numbers').split('/')
+             	    if number in numbers.split('/'):
                             memcache.incr(number)
-               else:
-                                   memcache.set('numbers', memcache.get('names') + '/' + number)
+            	    else:
+                                   memcache.set('numbers', numbers + '/' + number)
                                    memcache.set(number, 1)
-                                   numbers = memcache.get('names').split('/')
-                                   global primeNums
-                                   for n in numbers:
-                                          primeNums.append( (n, memcache.get(n) ) )
+            	numbers = memcache.get('numbers').split('/')
+              	global primeNums
+                for n in numbers:
+                           primeNums.append( (n, memcache.get(n) ) )
         return render_template('index.html', primeNums = primeNums, number = number, logouturl = logouturl, nickname = nickname)                                
     else:
       login_url = users.create_login_url('/')
